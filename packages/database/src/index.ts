568b639afb978107481c8767832a3428314e38a4
Marcos Vilalva <mvilalva87@gmail.com>
2026-06-28 16:13:18 -0300

    fix(database): load prisma environment automatically

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,5 +1,14 @@
-import 'dotenv/config';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { PrismaClient } from '@prisma/client';
+import dotenv from 'dotenv';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({
+    path: path.resolve(__dirname, '../.env')
+});
 
 export const prisma = new PrismaClient();
 
