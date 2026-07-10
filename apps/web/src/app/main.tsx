c602d3287420dc89368ba968f2cd78dadfd76b75
MarcosVilalva <mvilalva87@gmail.com>
2026-07-10 09:34:35 -0300

    feat(web): load catalogs styles

--- apps/web/src/app/main.tsx
+++ apps/web/src/app/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { AuthenticatedApp } from './AuthenticatedApp';
 import '../styles/global.css';
+import '../styles/catalogs.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
